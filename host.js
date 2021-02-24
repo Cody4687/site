9f18a70455e4b6388a3313d78377f9121d03ba6a
Cody <38409768+Cody4687@users.noreply.github.com>
2021-02-23 22:36:14 -0600

    Update host.js

--- host.js
+++ host.js
@@ -9,6 +9,6 @@ var server = require('http').createServer(app);
         res.sendFile(__dirname + 'index.html');
     });
 
-    server.listen(3000, function () {
-        console.log('listening on *:3000');
+    server.listen(3030, function () {
+        console.log('listening on *:3030');
     });
